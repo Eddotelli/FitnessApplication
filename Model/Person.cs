72402600fa8334bd971d6ae62a598db619fb7553
Edrisa Jallow <edrisa.j98@hotmail.com>
2024-10-17 11:09:09 +0200

    Status: Skapat upp strukturen för Person-klassen.

---
+++ Model/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitTrack.Model
+{
+    public abstract class Person
+    {
+        // ---------- Egenskaper ---------- //
+        public string Username {  get; set; }
+        public string Password { get; set; }
+
+        // ---------- Konstruktor ---------- //
+        public Person(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public void SignIn()
+        {
+
+        }
+
+    }
+}
